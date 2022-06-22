c02e2cc59b49b0546dff2c245ad2b79e56609c31
yeojining5 <yeojining5@gmail.com>
2022-06-22 15:20:20 +0900

    220622 HTML - table, semantic, jEasyUI-API

--- day1/hello.js
+++ day1/hello.js
@@ -1,8 +1,8 @@
 let colors = require("colors");
 // colors 변수 선언
 
-console.log(colors.rainbow("Hello NodeJS!!!"));
-console.log(colors.green("Hello NodeJS!!!"));
-console.log(colors.red("Hello NodeJS!!!"));
+console.log(colors.rainbow('Hello NodeJS!!!'));
+console.log(colors.green('Hello NodeJS!!!'));
+console.log(colors.red('Hello NodeJS!!!'));
 let a = 5;
 console.log(a);
