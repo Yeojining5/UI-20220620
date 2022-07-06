8eedecb1bcdcbfe3b9532e93b0e8688070f7dac8
yeojining5 <yeojining5@gmail.com>
2022-07-06 16:19:02 +0900

    220706 JS - html,css를 활용한 YoutubeAPI 가져오기

--- youtubeAPI-step1/mostPop.js
+++ youtubeAPI-step1/mostPop.js
@@ -1,7 +1,29 @@
-const ajax = new XMLHttpRequest();
-const MOST_URL = "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostpopular&maxresults=25&key=AIzaSyCC6kTd9ZieqFrFMVZaceC2-z6p0OjWSAY";
+/* String ver , +연산자 사용 */
+
+const ajax = new XMLHttpRequest(); // 인스턴스화
+let content = '';
+const MOST_URL = "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostpopular&maxresults=5&key=AIzaSyCC6kTd9ZieqFrFMVZaceC2-z6p0OjWSAY";
 ajax.open("GET", MOST_URL, false);
 ajax.send();
 
-console.log(ajax.response);
-/* const most = JSON */
+//console.log(ajax.response);
+const result = JSON.parse(ajax.response); // { ... items:[{snippet:...}]}
+const items = result.items; // Array
+console.log(items.length); // 25
+content += `<ul class='videos'>`
+for(let i=0;i<items.length;i++){
+  content += `<li class='container'>`
+  content += `<div class='video'>`
+  // console.log(items[i].snippet.title); // 타이틀 5개가 콘솔에 출력됨
+  // console.log(items[i].snippet.thumbnails.medium.url);
+  
+  content += `<img class='thumbnail' src='${items[i].snippet.thumbnails.medium.url}' alt='이미지'/>`;
+  content += `<div>`;
+  content += `<p class='title'>${items[i].snippet.title}</p>`; 
+  content += `<p class='channel'>${items[i].snippet.channelTitle}</p>`;
+  content += `</div>`;
+  content += `</div>`;
+}
+content += `</ul>`;
+document.querySelector('#root').innerHTML = content;
+
