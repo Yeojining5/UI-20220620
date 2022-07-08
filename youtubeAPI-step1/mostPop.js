f6a4a05135543d98f60298f383971d659377a319
yeojining5 <yeojining5@gmail.com>
2022-07-08 14:55:28 +0900

    220708 jEasyUI - Addressbook 실습

--- youtubeAPI-step1/mostPop.js
+++ youtubeAPI-step1/mostPop.js
@@ -4,7 +4,7 @@ const ajax = new XMLHttpRequest(); // 인스턴스화
 let content = '';
 const MOST_URL = "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostpopular&maxresults=5&key=AIzaSyCC6kTd9ZieqFrFMVZaceC2-z6p0OjWSAY";
 ajax.open("GET", MOST_URL, false);
-ajax.send();
+ajax.send(); // 지연이 발생할 수 있다.
 
 //console.log(ajax.response);
 const result = JSON.parse(ajax.response); // { ... items:[{snippet:...}]}
---
+++ 드림코딩/basic/operator.js
@@ -0,0 +1,4 @@
+// 1. String concatenation
+console.log('my' + 'cat');
+console.log('1' + 2); // 2를 string 으로 포함시켜버림
+console.log(`string literals: 1+2 = ${1+2}`);
