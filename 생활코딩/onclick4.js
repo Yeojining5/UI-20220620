250808ac4447f34d81cfc5ab27621e2303da8898
yeojining5 <yeojining5@gmail.com>
2022-07-18 15:06:44 +0900

    220718 JS - GX 예제 DOM 실습!!

---
+++ 생활코딩/onclick4.js
@@ -0,0 +1,7 @@
+const hw = document.getElementById('hw');
+    hw.addEventListener('click', function(){
+      alert('Hello world');
+    })
+    hw.addEventListener('click', () => alert('Hello world'));
+
+    
